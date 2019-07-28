4485c868ef1000c4ce74e389cf7e4e778bfe0581
gunkim0318 <gunkim0318@gmail.com>
2019-07-29 00:22:53 +0900

    파트2 진행중

--- ex01/src/main/java/org/zerock/config/ServletConfig.java
+++ ex01/src/main/java/org/zerock/config/ServletConfig.java
@@ -1,6 +1,10 @@
 package org.zerock.config;
 
+import java.io.IOException;
+
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -24,5 +28,21 @@ public class ServletConfig implements WebMvcConfigurer{
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	public CommonsMultipartResolver getResolver() throws IOException{
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		//10mb
+		resolver.setMaxUploadSize(1024 * 1024 * 10);
+		//2mb
+		resolver.setMaxUploadSizePerFile(1024 * 1024 * 2);
+		//1mb
+		resolver.setMaxInMemorySize(1024 * 1024);
+		
+		//temp upload
+		resolver.setUploadTempDir(new FileSystemResource("C:\\upload\\tmp"));
+		
+		resolver.setDefaultEncoding("UTF-8");
+		
+		return resolver;
 	}
 }  
--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -4,12 +4,19 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -65,5 +72,44 @@ public class SampleController {
 	public String ex03(TodoDTO dto) {
 		log.info("todo : "+dto);
 		return "ex03";
+	}
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto : "+dto);
+		log.info("page : "+page);
+		return "/sample/ex04";
+	}
+	@GetMapping("/ex06")
+	public @ResponseBody SampleDTO ex06() {
+		log.info("/ex06....");
+		
+		SampleDTO dto = new SampleDTO();
+		dto.setAge(10);
+		dto.setName("홍길동");
+		
+		return dto;
+	}
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		log.info("/ex07.....");
+		
+		String msg = "{\"name\": \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload...");
+	}
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+		files.forEach(file -> {
+			log.info("-------------------");
+			log.info("name : "+file.getOriginalFilename());
+			log.info("size : "+file.getSize());
+		});
 	}
 }
