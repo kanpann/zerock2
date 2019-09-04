cf118a8489a85db3fca0ca162e9c96fe161b8160
gunkim <gunkim0318@gmail.com>
2019-09-04 17:46:20 +0900

    페이징 처리 수정

--- ex02/src/main/java/org/zerock/domain/PageDTO.java
+++ ex02/src/main/java/org/zerock/domain/PageDTO.java
@@ -18,10 +18,10 @@ public class PageDTO {
         this.cri = cri;
         this.total = total;
 
-        this.endPage = (int) (Math.ceil(cri.getPageNum() / 10l)) * 10;
+        this.endPage = (int) (Math.ceil(cri.getPageNum() / 10.0)) * 10;
 
         this.startPage = this.endPage - 9;
-
+        
         int readEnd = (int) (Math.ceil((total * 1l) / cri.getAmount()));
 
         if(readEnd < this.endPage){
